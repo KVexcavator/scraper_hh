51403cea0fb35bbe9f4a0a4a887a1f01f4fa7078
kv-2 <str_token@mail.ru>
2019-04-14 22:31:22 +0300

    vhenge url to https://www.avito.ru/moskva/vakansii

--- scraper.rb
+++ scraper.rb
@@ -3,7 +3,7 @@ require 'byebug'
 require 'httparty'
 
 def scraper
-  url='https://hh.ru/'
+  url='https://www.avito.ru/moskva/vakansii/'
   unparsed_page=HTTParty.get(url)
   parsed_page=Nokogiri::HTML(unparsed_page)
   byebug
