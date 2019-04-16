cbb017de5204818d15787a142e8d4b75d390b7a3
kv-2 <str_token@mail.ru>
2019-04-16 21:00:05 +0300

    rename scraper

--- scraper_1.rb
+++ scraper_1.rb
@@ -4,9 +4,11 @@ require 'httparty'
 
 def scraper
   url='https://www.avito.ru/moskva/vakansii'
-  unparsed_page=HTTParty.get(url)
-  parsed_page=Nokogiri::HTML(unparsed_page)
+  p url
+  p unparsed_page=HTTParty.get(url)
+  p parsed_page=Nokogiri::HTML(unparsed_page)
   job_categori=parsed_page.css('div .rubricator-link-3w6_y')
+  categori_listing={}
   categori_listing.each do |job_categori|
     
   end
